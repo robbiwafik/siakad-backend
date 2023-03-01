0dafd9b1f26bfc5bd74a4197a26580d26811934a
Robbi Wafik <robbiwafik31@gmail.com>
2023-03-01 20:20:13 +0700

    Configure settings for managing files

--- siakad/settings.py
+++ siakad/settings.py
@@ -136,3 +136,6 @@ AUTH_USER_MODEL = 'core.User'
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- siakad/urls.py
+++ siakad/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     path('academic/', include('academic.urls')),
     path('__debug__', include('debug_toolbar.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
