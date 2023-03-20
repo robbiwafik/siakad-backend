ee249c6a0d2f25c3edfb5971cd6adb7788eb2426
Robbi Wafik <robbiwafik31@gmail.com>
2023-03-20 13:29:51 +0700

    Install djoser

--- siakad/urls.py
+++ siakad/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('academic/', include('academic.urls')),
+    path('auth/', include('djoser.urls')),
     path('__debug__', include('debug_toolbar.urls'))
 ]
 
